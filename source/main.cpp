a80441fb3a4d3f2a4d5256755553a8b7d3a317f1
Cee-Ry <eduaveceasar@gmail.com>
2026-04-18 23:15:15 +0800

    threaded loop

--- source/main.cpp
+++ source/main.cpp
@@ -1,11 +1,24 @@
 #include "../header/game.hpp"
+#include "../header/func.hpp"
+
 #include <iostream>
+#include <thread>
+#include <chrono>
+
+Functions func;
 
 int main() {
-    map.set();
-    snake.at();
-    map.show();
-    
+    func.hideCursor();
+
+    while (!snake.isDead) {
+        map.set();
+        snake.at();
+        map.show();
+        
+        std::this_thread::sleep_for(std::chrono::milliseconds(snake.speed));
+
+        func.clearScreen();
+    }
     
     return 0;
 }
