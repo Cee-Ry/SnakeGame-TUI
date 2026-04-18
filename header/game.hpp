01d433ab08235dcf93060a9c8e79b4e04da5b743
Cee-Ry <eduaveceasar@gmail.com>
2026-04-18 22:37:08 +0800

    made all struct call as extern

--- header/game.hpp
+++ header/game.hpp
@@ -4,14 +4,11 @@
 #include <string>
 
 struct Map {
-    static const int width = 60;
-    static const int height = 20;
+    static const int width = 80;
+    static const int height = 30;
     char map[height][width];
 
-    char borderL {'|'};
-    char borderR {'|'};
-    char borderU {'-'};
-    char borderD {'-'};
+    char border {'#'};
 
     void set();
     void show();
@@ -23,12 +20,12 @@ struct Snake {
     char SnakeBody {'*'};
     
     int headX {nav.width /2};
-    int headXx {nav.width /2};
     int headY {nav.height /2};
     
     void at();
 };
 
 extern Map map;
+extern Snake snake;
 
 #endif
