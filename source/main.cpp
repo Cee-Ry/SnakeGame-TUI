ad04fe43204b34647902504a6f4121778868dbd7
Cee-Ry <eduaveceasar@gmail.com>
2026-04-21 22:05:17 +0800

    uses ncurses for better terminal control

--- source/main.cpp
+++ source/main.cpp
@@ -1,16 +1,26 @@
 #include "../headers/map.hpp"
+#include "../headers/snake.hpp"
 #include <iostream>
+#include <ncurses.h>
 
 int main() {
+    initscr();
     
     map.set();
 
     while (true) {
+        snake.goTo();
         map.update();
         map.display();
+        std::cout << "X: " << snake.headX -1 << 
+            "Y: " << snake.headY - 1 << "\n\r";
 
         std::cin.get();
+
+        clear();
+        refresh();
     }
+    endwin();
 
     return 0;
 }
