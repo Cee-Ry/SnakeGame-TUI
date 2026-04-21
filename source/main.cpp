69248f4152a08d21e0a96be4c63b3dc328b9e690
Cee-Ry <eduaveceasar@gmail.com>
2026-04-21 22:43:47 +0800

    fully functional but with bug

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,10 @@
 #include "../headers/map.hpp"
 #include "../headers/snake.hpp"
-#include <iostream>
 #include <ncurses.h>
 
 int main() {
     initscr();
+    curs_set(0);
     
     map.set();
 
@@ -12,14 +12,12 @@ int main() {
         snake.goTo();
         map.update();
         map.display();
-        std::cout << "X: " << snake.headX -1 << 
-            "Y: " << snake.headY - 1 << "\n\r";
-
-        std::cin.get();
+        snake.nav();
 
         clear();
         refresh();
     }
+
     endwin();
 
     return 0;
