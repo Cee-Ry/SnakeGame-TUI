ff9ae739080fe16ef9c4bdef3eea9f00b853275f
Cee-Ry <eduaveceasar@gmail.com>
2026-04-22 21:37:21 +0800

    fixed the phasing bug/glitch(idk what to call)

--- headers/map.hpp
+++ headers/map.hpp
@@ -4,8 +4,8 @@
 #include <string>
 
 struct Map {
-    static const int height = 25;
-    static const int width = height * 2 + (height * 0.30);
+    static const int height = 30;
+    static const int width = 60;
 
     std::string outline[height][width];
     std::string wall = "\033[41;31m#\033[0m";
