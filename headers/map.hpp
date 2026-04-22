64f261230bbe3691c474ef455115ffb9d3fcbc94
Cee-Ry <eduaveceasar@gmail.com>
2026-04-22 15:16:39 +0800

    automate mapping

--- headers/map.hpp
+++ headers/map.hpp
@@ -4,8 +4,8 @@
 #include <string>
 
 struct Map {
-    static const int width = 52;
-    static const int height = 20;
+    static const int height = 25;
+    static const int width = height * 2 + (height * 0.30);
 
     std::string outline[height][width];
     std::string wall = "\033[41;31m#\033[0m";
