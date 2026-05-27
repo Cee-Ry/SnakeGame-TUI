7452492c6247dae7eda173f922abf49eb6d8451f
Cee-Ry <eduaveceasar@gmail.com>
2026-05-27 12:54:21 +0800

    restarting

--- source/main.cpp
+++ source/main.cpp
@@ -1,29 +1,9 @@
-#include "../headers/map.hpp"
-#include "../headers/snake.hpp"
-#include "../headers/food.hpp"
 #include <ncurses.h>
-#include <iostream>
 
 int main() {
     initscr();
-    curs_set(0);
-    
-    map.set();
-
-    while (snake.inGame) {
-        std::cout << "X: " << food.X << "\tY: " << food.Y << "\n\rscore: " << food.score << "\n\r";
-        snake.goTo();
-        map.update();
-        map.display();
-        // std::cout << "Y: " << snake.headY <<
-            // "\tX: " << snake.headX << "\n\r";
-        snake.nav();
-
-        clear();
-        refresh();
-    }
+    getch();
 
     endwin();
-
     return 0;
 }
